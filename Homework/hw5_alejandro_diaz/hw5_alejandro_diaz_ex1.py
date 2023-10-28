256abba6ec8b3b48a211dbd97220cbf64948b4f1
Alejandro Diaz <alexdiaz0923@gmail.com>
2023-10-28 10:02:17 -0600

    Created hw5 files

---
+++ Homework/hw5_alejandro_diaz/hw5_alejandro_diaz_ex1.py
@@ -0,0 +1,24 @@
+"""
+Homework 5 Exercise 1
+Name: Alejandro Diaz
+Due Date: 11/09/23
+
+Exercise 1:
+Write an iterator class ReverseIter, that takes a list and iterates it from the reverse direction.
+An example could be like this (hint – use a list as the input argument of your constructor):
+
+    # >>> it = ReverseIter([1, 2, 3, 4])
+    # >>> next(it)
+    # 4
+    # >>> next(it)
+    # 3
+    # >>> next(it)
+    # 2
+    # >>> next(it)
+    # 1
+    # >>> next(it)
+    # Traceback (most recent call last):
+    # File "<stdin>", line 1, in <module>
+    # StopIteration
+
+"""
---
+++ Homework/hw5_alejandro_diaz/hw5_alejandro_diaz_ex3.py
@@ -0,0 +1,18 @@
+"""
+Homework 5 Exercise 3
+Name: Alejandro Diaz
+Due Date: 11/09/23
+
+Exercise 3:
+Write three Python classes named Rectangle constructed by a length and width, a Circle
+constructed by a radius, and a Square constructed by a side length. All classes should have the
+methods that compute:
+
+    - The area
+    - The diagonal of the Rectangle or Square class, and the diameter of the Circle class
+    - The perimeter
+
+Use as much inheritance as you can. Import the math module, so you can call methods like sqrt().
+At the end of the code, use these classes and methods to calculate the perimeter of a circle with
+a radius that’s half of the diagonal of a rectangle with a length of 20 and width 10.
+"""
