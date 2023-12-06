3024abd0e96b6aa7e8c073031e9eb22968dc0db3
Alejandro Diaz <alexdiaz0923@gmail.com>
2023-12-06 15:51:36 -0700

    .

---
+++ Personal/pythonProject/matrix_functions.py
@@ -0,0 +1,53 @@
+import math
+import numpy as np
+
+
+# Matrix used to translate a vertex in space to a given position tx, ty, tz:
+def translate(pos):
+    tx, ty, tz = pos
+    return np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [tx, ty, tz, 1]
+    ])
+
+
+# Matrix used to rotate a vertex around the x-axis by a given amount of degrees:
+def rotate_x(a):
+    return np.array([
+        [1, 0, 0, 0],
+        [0, math.cos(a), math.sin(a), 0],
+        [0, -math.sin(a), math.cos(a), 0],
+        [0, 0, 0, 1]
+    ])
+
+
+# Matrix used to rotate a vertex around the y-axis by a given amount of degrees:
+def rotate_y(a):
+    return np.array([
+        [math.cos(a), 0, -math.sin(a), 0],
+        [0, 1, 0, 0],
+        [math.sin(a), 0, math.cos(a), 0],
+        [0, 0, 0, 1]
+    ])
+
+
+# Matrix used to rotate a vertex around the z-axis by a given amount of degrees:
+def rotate_z(a):
+    return np.array([
+        [math.cos(a), math.sin(a), 0, 0],
+        [-math.sin(a), math.cos(a), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+# Matrix used to scale a vertex in space by a given amount:
+def scale(n):
+    return np.array([
+        [n, 0, 0, 0],
+        [0, n, 0, 0],
+        [0, 0, n, 0],
+        [0, 0, 0, 1]
+    ])
